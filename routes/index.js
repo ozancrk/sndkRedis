1aca74cfba3b75e93bdf0acbb725df661d51ffec
ozancrk <ozancrk@gmail.com>
2023-09-03 00:07:20 +0300

    anasayfa ve categoriler

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const PostsFn = require("./posts.js");
 const HomeFn = require("./home.js");
 const CategoryFn = require("./categories.js");
 const LastpostsFn = require("./lastposts.js");
-const PagesFn = require("./lastposts.js");
+const PagesFn = require("./pages.js");
 
 const RouterArrFn = [
     PostsFn,
--- routes/pages.js
+++ routes/pages.js
@@ -6,27 +6,22 @@ module.exports = (router) => {
 
         const url = req.params.url;
 
+        console.log(url)
+
         const KEY = 'page:' + url
         let value = await client.json.get(KEY)
 
         if (!value) {
             let data = await wp.pages().slug(url).get();
-            let val = {
-                'title': data.title.rendered,
-                'content': data.content.rendered,
-            }
-            let t = new Date(data.modified.replace(' ', 'T') + 'Z').getTime() / 1000;
-            if ((Date.now() / 1000) - t < 60480000 || val.cache) {
-                await client.json.set(KEY, '$', val)
-                await client.expire(KEY, process.env.CACHETTL)
-            }
+            await client.json.set(KEY, '$', data)
+            await client.expire(KEY, process.env.CACHETTL)
             res.json({
-                page: url, postData: val,
+                page: url, postData: data,
             })
 
         } else {
             res.json({
-                page: url, postData: value,
+                page: url, postData: data,
             })
         }
     })
