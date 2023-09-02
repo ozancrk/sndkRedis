1c593bec830bec3d198a0de36bfebf38e7d21719
ozancrk <ozancrk@gmail.com>
2023-09-02 20:10:46 +0300

    anasayfa ve categoriler

--- routes/categories.js
+++ routes/categories.js
@@ -5,26 +5,23 @@ module.exports = (router) => {
     router.get("/category/:uid/:limit?", async (req, res) => {
 
         let redis = true;
-        let value;
+        let posts;
         const KEY = 'category:' + req.params.uid;
 
-        value = await client.json.get(KEY)
-        if (!value) {
+        posts = await client.json.get(KEY)
+        if (!posts) {
             let posts = await wp.posts()
                 .categories(req.params.uid)
                 .perPage(req.params.limit)
                 .get();
-            value = {
-                posts
-            }
-            await client.json.set(KEY, '$', value)
+            await client.json.set(KEY, '$', posts)
             await client.expire(KEY, process.env.CategoryCACHETTL)
             redis = false
         }
 
 
         res.json({
-            value,
+            posts,
             redis
         })
 
