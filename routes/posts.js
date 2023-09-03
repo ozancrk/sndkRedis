59179adcb83488ace8a0d53e52819ef4f8889fd0
ozancrk <ozancrk@gmail.com>
2023-09-03 20:34:38 +0300

    anasayfa ve categoriler

--- routes/posts.js
+++ routes/posts.js
@@ -30,7 +30,8 @@ module.exports = (router) => {
                     'excerpt': data.excerpt.rendered,
                     'time': data.modified,
                     'media': data.mediaURL,
-                    'customMeta': data.customMeta
+                    'customMeta': data.customMeta,
+                    'postURL': data.postURL
                 }
 
                 // POSTUN TARİHİ KONTROL EDİLİYOR
